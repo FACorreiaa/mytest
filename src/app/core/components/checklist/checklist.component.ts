b5c3b963d0a7e40f11c5cdca3575240435a09e81
Patrick Theuerweckl <ptheuerweckl@cocus.com>
2019-06-11 17:20:15 +0200

    feat(CS-764): merged changes for openTime

--- src/app/core/components/checklist/checklist.component.ts
+++ src/app/core/components/checklist/checklist.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core'
+import { ICategoryDto } from '@app/api/models/api-models'
 
 @Component({
     selector: 'checklist',
