95c41c5caf0ec625842b28891159a504825177d1
Patrick Theuerweckl <ptheuerweckl@cocus.com>
2019-06-28 17:12:29 +0200

    feat(CS-957): gmb transfer dialog
    include the styles for the transfer dialog and the success, error
    messages

---
+++ src/app/main/components/error-dialog/error-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'error-dialog',
+    templateUrl: 'error-dialog.component.html',
+    styleUrls: ['./error-dialog.component.scss'],
+})
+export class ErrorDialogComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -22,6 +22,9 @@ import { CookieSettingsComponent } from './components/cookie-settings/cookie-set
 import { DeleteAccComponent } from './components/delete-acc/delete-acc.component'
 import { DeleteConfirmComponent } from './components/delete-confirm/delete-confirm.component'
 import { StatusLogComponent } from './containers/status-log/status-log.component'
+import { TransferGmbComponent } from './components/transfer-gmb/transfer-gmb.component'
+import { SendInviteComponent } from './components/send-invite/send-invite.component'
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component'
 
 @NgModule({
   imports: [
@@ -47,9 +50,19 @@ import { StatusLogComponent } from './containers/status-log/status-log.component
     ImprintComponent,
     DataPrivacyComponent,
     CookieSettingsComponent,
-    StatusLogComponent
+    StatusLogComponent,
+    TransferGmbComponent,
+    SendInviteComponent,
+    ErrorDialogComponent
   ],
-  entryComponents: [CookieSettingsComponent, DeleteAccComponent, DeleteConfirmComponent],
+  entryComponents: [
+    CookieSettingsComponent,
+    DeleteAccComponent,
+    DeleteConfirmComponent,
+    TransferGmbComponent,
+    SendInviteComponent,
+    ErrorDialogComponent
+  ],
   exports: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
